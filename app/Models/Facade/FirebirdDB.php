78d658aac3fc3619329ba6973740a686a202d4b8
renananchieta <renangap18@gmail.com>
2024-06-24 21:03:10 -0300

    Exportar CSV

--- app/Models/Facade/FirebirdDB.php
+++ app/Models/Facade/FirebirdDB.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Facade;
 
+use App\Http\Resources\Catalogo\CatalogoResource;
 use App\Models\Firebird;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Response;
@@ -24,18 +25,18 @@ class FirebirdDB
     public static function exportarCsv($params)
     {
         $data = self::grid($params);
+        $dados = CatalogoResource::collection($data);
 
         $file = fopen('php://temp', 'w+');
 
         fputcsv($file, ['ID', 'Nome', 'Embalagem Abreviada', 'Preço']);
 
-        foreach ($data as $row) {
-            dd($row->id);
+        foreach ($dados as $row) {
             fputcsv($file, [
-                $row->ID,
-                $row->NOME,
-                $row->EMB_ABREVIADA,
-                $row->PRECO
+                $row->id,
+                $row->nome,
+                $row->emb_abreviada,
+                $row->preco
             ]);
         }
 
