dba95b19a5c4683e334898bd00142676986fa79a
renananchieta <renangap18@gmail.com>
2024-07-08 02:14:57 -0300

    literatura teste

--- resources/views/produto/literatura_pdf.blade.php
+++ resources/views/produto/literatura_pdf.blade.php
@@ -6,55 +6,57 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LITERATURA E DETALHES DE PRODUTO</title>
     <style>
-        /* Estilos anteriores aqui */
-
-        .details-container {
+        #header {
             display: flex;
             justify-content: space-between;
-            margin-top: 20px;
-            page-break-inside: avoid; /* Evitar quebrar a página no meio de um detalhe */
+            align-items: center;
         }
 
-        .details-left {
-            width: 30%;
+        #header img {
+            margin: 0; /* Remova o espaçamento padrão das imagens */
+        }
+
+        @font-face {
+            font-family: 'Calibri';
+            src: url('<?= asset('fonts/calibri/calibri.ttf') ?>') format("truetype");
+            font-weight: normal;
+            font-style: normal;
+        }
+
+        @font-face {
+            font-family: 'CalibriB';
+            src: url('<?= asset('fonts/calibri/calibrib.ttf') ?>') format("truetype");
             font-weight: bold;
-            font-family: 'CalibriB', Arial, sans-serif; /* Utilizando a fonte Calibri bold se disponível */
-            text-align: left;
         }
 
-        .details-right {
-            width: 65%;
-            font-family: 'Calibri', Arial, sans-serif; /* Utilizando a fonte Calibri se disponível */
-            text-align: justify;
-            padding-left: 10px;
+        body {
+            font-family: Arial, sans-serif; /* Alterando para Arial */
+            font-size: 14px;
+            font-weight: bold;
+            text-align: center;
+            margin-bottom: 0;
         }
 
         /* Outros estilos continuam aqui */
+
     </style>
 </head>
 
 <body>
-    <div id="header">
-        <div>
+<div id="header">
+    <div>
+        <div> 
             @foreach ($literatura as $item)
                 <div>
                     <h2>{{ $item->PRD_NOME }}</h2>
                     <h3>"{{ $item->PRD_LIT_DSC }}"</h3>
                 </div>
-
-                {{-- @foreach ($item->detalhes as $detalhe)
-                    @foreach ($$detalhe as $desc)
-                        <div class="details-container">
-                            <div class="details-left">{{ $desc->LITENS_DSC }}</div>
-                            <div class="details-right">{{ $desc->LID_DSC }}</div>
-                        </div>
-                    @endforeach
-                @endforeach --}}
             @endforeach
         </div>
     </div>
+</div>
 
-    <!-- Restante do seu conteúdo HTML -->
+<!-- Restante do seu conteúdo HTML -->
 
 </body>
 
