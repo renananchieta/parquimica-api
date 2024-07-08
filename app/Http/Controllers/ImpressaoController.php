c5f82fe54c33e22fc86d9854d75d070c598063df
renananchieta <renangap18@gmail.com>
2024-07-07 23:56:40 -0300

    literatura teste

--- app/Http/Controllers/ImpressaoController.php
+++ app/Http/Controllers/ImpressaoController.php
@@ -11,7 +11,7 @@ class ImpressaoController extends Controller
     public function gerarPDF(Request $request)
     {
         $p = (object)$request->all();
-        dd($p);
+        return response($p);
 
         if (isset($p->imprime_literatura)){
             $literatura = FirebirdDB::literatura($p);
