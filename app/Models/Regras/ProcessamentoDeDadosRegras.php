1f67fb0a87f69ad3cb591eaf96d9db8a30499649
renananchieta <renangap18@gmail.com>
2024-08-10 21:00:43 -0300

    retornando mensagem de erro ao tentar salvar o produto

--- app/Models/Regras/ProcessamentoDeDadosRegras.php
+++ app/Models/Regras/ProcessamentoDeDadosRegras.php
@@ -14,7 +14,7 @@ class ProcessamentoDeDadosRegras
     {
         foreach($data as $produto) {
             // return (array) $produto;
-            dd($produto);
+            dd($produto->id);
 
             $codigo_produto = $produto->id;
 
