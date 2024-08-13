29c8ecf13eaf644c205e663489e00f7a656f7d77
renananchieta <renangap18@gmail.com>
2024-08-12 22:39:10 -0300

    Job para salvar produtos

--- app/Models/Regras/ProcessamentoDeDadosRegras.php
+++ app/Models/Regras/ProcessamentoDeDadosRegras.php
@@ -153,7 +153,7 @@ class ProcessamentoDeDadosRegras
             /**
              * Aqui dentro que vai ser feito o salvamento do produto
              */
-            dd($groupedLiteraturas[0]);
+            dd($groupedLiteraturas);
             $produto = (array)$groupedLiteraturas[0];
 
             $codigo_produto = $produto['PRD_COD'];
