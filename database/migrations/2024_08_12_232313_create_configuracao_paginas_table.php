dbd299873c3b1b42d3d7316f7974972f0bc1b94c
renananchieta <renangap18@gmail.com>
2024-08-12 23:52:51 -0300

    Job para salvar produtos

---
+++ database/migrations/2024_08_12_232313_create_configuracao_paginas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('configuracao_paginas', function (Blueprint $table) {
+            $table->id();
+            $table->string('descricao');
+            $table->text('texto');
+            $table->binary('arquivo');
+            $table->boolean('status_publicacao')->default(false);
+            $table->string('slug')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('configuracao_paginas');
+    }
+};
---
+++ database/migrations/2024_08_12_233132_create_configuracao_blog_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('configuracao_blog', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->text('texto');
+            $table->binary('arquivo');
+            $table->string('categoria');
+            $table->dateTime('data_publicacao');
+            $table->boolean('status_publicacao')->default(false);
+            $table->string('slug')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('configuracao_blog');
+    }
+};
