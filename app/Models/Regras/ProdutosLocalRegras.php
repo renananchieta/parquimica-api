3c4c4a4c379510afaa7814bfacf6b975b2f96811
renananchieta <renangap18@gmail.com>
2024-08-26 21:37:46 -0300

    atualizar produto para status ativo

--- app/Models/Regras/ProdutosLocalRegras.php
+++ app/Models/Regras/ProdutosLocalRegras.php
@@ -110,7 +110,7 @@ class ProdutosLocalRegras
     public static function atualizarProdutoAtivoSite( array $data)
     {
         foreach($data as $produto) {
-            $produtoUpdt = ProdutosLocal::where('codigo_produto', $produto->ID);
+            $produtoUpdt = ProdutosLocal::where('codigo_produto', $produto->ID)->first();
             $produtoUpdt->ativo_site = $produto->ATIVO_SITE;
             $produtoUpdt->save();
 
