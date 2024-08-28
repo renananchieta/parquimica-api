fd90b46d54181894d4965fe81c21934be1413549
renananchieta <renangap18@gmail.com>
2024-08-28 00:17:44 -0300

    restaurar banco

--- database/migrations/2024_08_04_193220_create_produtos_table.php
+++ database/migrations/2024_08_04_193220_create_produtos_table.php
@@ -13,12 +13,17 @@ return new class extends Migration
     {
         Schema::create('produtos', function (Blueprint $table) {
             $table->id();
-            $table->integer('codigo_produto')->unique();
-            $table->string('nome_produto')->unique();
+            $table->integer('codigo_produto');
+            $table->string('nome_produto');
             $table->text('subtitulo');
             $table->text('modo_acao');
             $table->text('recomendacao')->nullable();
             $table->string('slug')->nullable();
+            $table->integer('ativo_site')->default(0);
+            $table->text('variantes')->nullable();
+            $table->string('caminho_arquivo')->nullable();
+            $table->string('linha')->nullable();
+            $table->string('funcao')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
